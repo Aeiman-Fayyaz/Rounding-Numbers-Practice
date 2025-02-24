5bdacba6c2a959a29d49931816d414932a569930
Aeiman Fayyaz <aeimanfayyaz2002@gmail.com>
2025-02-24 12:48:37 +0500

    new code

---
+++ app.js
@@ -0,0 +1,11 @@
+let numRandom = (Math.random()*6)+1
+
+let userToss = prompt("Roll a dice")
+
+if(numRandom === 6){
+    document.write("You wins")
+}
+else(document.write("Try again"))
+
+// console.log(Math.floor(numRandom));
+
