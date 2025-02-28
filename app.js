b595dead65823da5800121718cd205c6be941f14
Aeiman Fayyaz <aeimanfayyaz2002@gmail.com>
2025-02-28 13:16:43 +0500

    code update

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
 
 // console.log(Math.floor(numRandom));
 
-let timeToday = new Date()
+/*let timeToday = new Date()
 
 let week = timeToday.getDay()
 let month = timeToday.getMonth()
@@ -31,4 +31,50 @@ let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'A
 
 document.write("<h4>Today's Time:</h4>" + "<br/>" + days[week] + " " + months[month] + " " + date + " " + year + " " + hours + ":" + minutes + ":" + seconds )
 
-console.log(timeToday);
+console.log(timeToday);*/
+
+
+// Age Calculator
+
+let userName = prompt("Enter your Name")
+
+let todayDate = new Date().getFullYear()
+
+let userAge = prompt("Enter your birthday")
+
+// User Age
+let userBirthdayYear = new Date(userAge).getFullYear()
+
+let age = (todayDate - userBirthdayYear);
+
+// Getting MilliSeconds from birthday to today
+let todayMs = new Date()
+
+let todayMilliSec = todayMs.getTime()
+
+let userDaysPassMilliSec = new Date(userAge).getTime()
+
+// Difference
+let difference = (todayMilliSec - userDaysPassMilliSec);
+
+// // Days Pass
+// let currentMilliSec = Math.round( difference / (1000 * 60 * 60 *24))
+
+// Getting Seconds From brithday to today
+let todaySeconds = Math.round(difference / (1000 * 60))
+
+// Getting Minutes From brithday to today 
+let todayMinutes = Math.round(difference / (1000 * 60 * 60))
+
+// Getting Hours From Birthday to today
+let todayHours = Math.round(difference / (1000 * 60 * 60 * 24))
+
+// Getting weeks from Birthday to Today
+let weekToday = Math.round(difference / (1000 * 60 * 60 * 24 * 7))
+
+document.write("<h1>Age Calculator</h1>" + "<h4>Name:</h4>" + userName + "<br/>" + "<h4>Your Birthday:</h4>" + 
+userAge +  "<h4>Current Year:</h4>" + todayDate + "<h4>Your Age:</h4>" + age + "<h4>Today MilliSeconds:</h4>" + todayMilliSec + "<br/>" + 
+"<h4>MilliSeconds Pass from Your Birthday to Today:</h4>" + "<br/>" + userDaysPassMilliSec + "<h4>Seconds Pass from your Brithday to Today:</h4>" + todaySeconds +
++ "<h4>Minutes Pass from Your Birthday to Today:</h4>" + todayMinutes + "<h4>Hours Pass from Birthday to Today:</h4>" + todayHours + 
+"<h4>Week Pass from Your Birthday to Today:</h4>" + weekToday )
+// "<h4>Days Pass:</h4>" + currentMilliSec
